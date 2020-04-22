7a6c2a4a77852d94354b8472bb673ecf665c0998
Nguyễn Huỳnh Sáng <35994204+NguyenHuynhSang@users.noreply.github.com>
2020-04-22 16:39:37 +0700

    tích hợp admin template, angularjs, angular router-ui

---
+++ eshopcore/src/main/webapp/app/components/product/products-module.js
@@ -0,0 +1,19 @@
+﻿/// <reference path="../assets/admin/libs/plugins/angular/angular.js" />
+
+(function () {
+    angular.module('eshop-product', ['eshop-common']).config(config);
+    config.$inject = ['$stateProvider', '$urlRouterProvider'];
+    function config($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('product-edit', {
+            url: "/product-edit",
+            templateUrl: "./app/components/product/product-edit-view.html",
+            controller: "product-edit-controller"
+        }).state('product-list', {
+            url: "/product-list",
+            templateUrl: "./app/components/product/product-list-view.html",
+            controller: "product-list-controller"
+        });
+    }
+
+})();
+
