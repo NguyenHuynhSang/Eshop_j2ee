a67f10a586c483af649bc2d7888ac88aae735888
Minh <16520742@gm.uit.edu.vn>
2020-04-28 20:42:28 +0700

    TestJSon
    TestJSon ( doget)

---
+++ eshopcore/src/main/java/Api/JSonAPI.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Api;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import Service.JSonService;
+import Model.JSon;
+import com.google.gson.Gson;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@WebServlet(name = "JSonAPI", urlPatterns = {"/JSonAPI"})
+public class JSonAPI extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+               response.setContentType("application/json");
+        JSonService jsonservice = new JSonService();       
+        List<JSon> json= new ArrayList<JSon>();
+        Gson gson=new Gson();
+        
+        try {
+            json = jsonservice.GetAllJSon();
+        } catch (SQLException ex) {
+            Logger.getLogger(JSonAPI.class.getName()).log(Level.SEVERE, null, ex);
+        }
+ 
+       PrintWriter printWriter=response.getWriter();
+       
+       printWriter.print(gson.toJson(json));
+    }
+ 
+
+}
---
+++ eshopcore/src/main/java/Service/JSonService.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Service;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import Model.JSon;
+/**
+ *
+ * @author nhatminh
+ */
+public class JSonService {
+    
+     Connection conn = null;
+     
+     public List<JSon> GetAllJSon() throws SQLException
+    {
+        DbConnection db = new DbConnection();
+        conn = db.getJDBCConnection();
+        
+        List<JSon> jsons= new ArrayList<JSon>();
+
+        ResultSet rs = null;
+        Statement stmt;
+        stmt = conn.createStatement();
+        String sqlQuery="SELECT * FROM JSon";
+        rs = stmt.executeQuery(sqlQuery);
+        JSon json = null;
+        while(rs.next())
+        {   
+             int id=rs.getInt("ID");
+             String name = rs.getString("Name");
+             json = new JSon(id,name);
+             jsons.add(json);
+        }
+        return jsons;
+    }  
+}
