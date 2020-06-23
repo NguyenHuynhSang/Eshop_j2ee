13bd8f8b0862ad7d90354b6c5d4303f15f88a9e7
NguyenHuynhSang <dev4fun.git@gmail.com>
2020-06-23 10:43:52 +0700

    fix lost image input value

--- eshopcore/src/main/webapp/app/components/content/content-create-controller.js
+++ eshopcore/src/main/webapp/app/components/content/content-create-controller.js
@@ -10,6 +10,8 @@
         $scope.keyWord = '';
 
         function CreateContent() {
+            var urlReturn=$('#txtImage').val();
+            $scope.contentEntity.Image=urlReturn;
             apiService.post('/eshopcore/API-Content', JSON.stringify($scope.contentEntity)  , function (result) {
                 notificationService.displaySuccess("Thêm mới bản ghi thành công");
                 $state.go('content-list');
