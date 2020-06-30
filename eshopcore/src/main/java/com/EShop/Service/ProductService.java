efb372bb17e1bfdae2b1d23f344c5b067c28251c
NguyenHuynhSang <dev4fun.git@gmail.com>
2020-06-30 19:32:45 +0700

    script db

--- eshopcore/src/main/java/com/EShop/Service/ProductService.java
+++ eshopcore/src/main/java/com/EShop/Service/ProductService.java
@@ -9,7 +9,7 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.List;
 
-public class ProductService  implements IProductService {
+public class ProductService implements IProductService {
     Connection conn = DbConnection.getJDBCConnection();
 
     @Override
@@ -25,9 +25,9 @@ public class ProductService  implements IProductService {
     @Override
     public void InsertProduct(ProductInput product) throws SQLException {
         Statement statement;
-        String sqlQuery="select * \n" +
+        String sqlQuery = "select * \n" +
                 "from Product\n" +
-                "where Name='"+product.Name+ "'"; // check trùng
+                "where Name='" + product.Name + "'"; // check trùng
         ResultSet rs;
         conn.setAutoCommit(false);
         statement = conn.createStatement();
@@ -35,22 +35,21 @@ public class ProductService  implements IProductService {
 
         String createdDate = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'").format(Calendar.getInstance().getTime());
         rs = statement.executeQuery(sqlQuery);
-        if (rs.next() == false)
-        {
+        if (rs.next() == false) {
 
-            sqlQuery="Insert into Product (CatalogID,Url,Name,Description,Content,CreatedDate,CreatedBy,Weight,OriginalPrice,Deliver,SEOTitle,SEOUrl,SEODescription,ApplyPromotion) \n" +
+            sqlQuery = "Insert into Product (CatalogID,Url,Name,Description,Content,CreatedDate,CreatedBy,Weight,OriginalPrice,Deliver,SEOTitle,SEOUrl,SEODescription,ApplyPromotion) \n" +
                     "values (?, ?, ?, ?,?,?,?,?,?,?,?,?,?,?)";
-            String columnNames[] = new String[] { "ID" };
+            String columnNames[] = new String[]{"ID"};
 
-            PreparedStatement pStatement=conn.prepareStatement(sqlQuery,Statement.RETURN_GENERATED_KEYS);
+            PreparedStatement pStatement = conn.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
             //statement = conn.prepareStatement(sqlQuery);
-            pStatement.setObject(1,product.CatalogID);
+            pStatement.setObject(1, product.CatalogID);
             pStatement.setObject(2, product.Url);
             pStatement.setObject(3, product.Name);
             pStatement.setObject(4, product.Description);
             pStatement.setObject(5, product.Content);
             pStatement.setObject(6, createdDate);
-            pStatement.setObject(7,product.CreatedBy);
+            pStatement.setObject(7, product.CreatedBy);
             pStatement.setObject(8, product.Weight);
             pStatement.setObject(9, product.OriginalPrice);
             pStatement.setObject(10, product.Deliver);
@@ -58,17 +57,19 @@ public class ProductService  implements IProductService {
             pStatement.setObject(12, product.SEOUrl);
             pStatement.setObject(13, product.SEODescription);
             pStatement.setObject(14, product.ApplyPromotion);
-            int rowCount=pStatement.executeUpdate();
-            ResultSet rss=pStatement.getGeneratedKeys();
-            int  last_inserted_id=0;
-            if (rss.next())
-            {
-               int productID = Integer.parseInt(rss.getObject(1).toString()) ;
+            int rowCount = pStatement.executeUpdate();
+            ResultSet rss = pStatement.getGeneratedKeys();
+            int last_inserted_id = 0;
+            if (rss.next()) {
+                int productID = Integer.parseInt(rss.getObject(1).toString());
+                sqlQuery = "Insert into Product (CatalogID,Url,Name,Description,Content,CreatedDate,CreatedBy,Weight,OriginalPrice,Deliver,SEOTitle,SEOUrl,SEODescription,ApplyPromotion) \n" +
+                        "values (?, ?, ?, ?,?,?,?,?,?,?,?,?,?,?)";
+                PreparedStatement verStatement = conn.prepareStatement(sqlQuery, Statement.RETURN_GENERATED_KEYS);
+
+
             }
             pStatement.close();
-        }
-        else
-        {
+        } else {
             System.out.println("Đã tồn tại sản phẩm");
         }
         rs.close();
