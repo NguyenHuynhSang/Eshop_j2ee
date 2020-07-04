9cdfb2f6be690e30516448e1ccb45678c1382c41
NguyenHuynhSang <dev4fun.git@gmail.com>
2020-07-04 16:28:12 +0700

    update cart

--- eshopcore/src/main/webapp/Client_app/components/cart/view-cart-controller.js
+++ eshopcore/src/main/webapp/Client_app/components/cart/view-cart-controller.js
@@ -6,5 +6,31 @@
     //chú ý thứ tự
     function viewCartController($scope, apiService, notificationService, $ngBootbox) {
 
+
+        $scope.cart={};
+
+        $scope.getCart=getCart;
+
+
+        function getCart() {
+
+            /*Cấu trúc config cho doget để get ra parameter chú ý các tên action*/
+            apiService.get('/eshopcore_war/api/CartApi', null, function (result) {
+                $scope.cart = result.data;
+                if (result.data.length == 0) {
+                    notificationService.displayWarning("Không tìm thấy slides nào");
+                } else {
+                    notificationService.displaySuccess("[DEV]Tìm đước slides");
+                }
+            }, function () {
+                notificationService.displayError("Không lấy được dữ liệu từ server");
+            });
+
+
+        }
+
+        $scope.getCart();
+
+
     }
 })(angular.module('eshop-cart'));
