8810f5a06377cbf02d1527763a916f3e6dd05f02
NguyenHuynhSang <dev4fun.git@gmail.com>
2020-07-26 16:49:38 +0700

    fix order service

--- eshopcore/src/main/webapp/app/components/order/order-list-controller.js
+++ eshopcore/src/main/webapp/app/components/order/order-list-controller.js
@@ -5,7 +5,45 @@
 
 //chú ý thứ tự
     function orderListListController($scope, apiService, notificationService, $ngBootbox) {
-        
+
+        $scope.ordersList=[];
+        $scope.productFilter={};
+
+        $scope.getListOrders=getListOrders;
+        function getListOrders() {
+            /*Cấu trúc config cho doget để get ra parameter chú ý các tên action*/
+            var config = {
+                params: {
+                    filterJson: angular.toJson($scope.productFilter)
+                }
+            }
+            apiService.get('/eshopcore_war/api/invoice', null, function (result) {
+                $scope.ordersList = result.data;
+                if (result.data.length == 0) {
+                    notificationService.displayWarning("Không tìm thấy bản ghi nào");
+                } else {
+
+                    notificationService.displaySuccess("Tìm thấy " + result.data.length + " bản ghi");
+                }
+
+
+            }, function () {
+                console.log('Load product api failed.');
+                notificationService.displayError("Không lấy được dữ liệu từ server");
+            });
+
+        }
+
+
+
+
+
+
+        $scope.getListOrders();
+
+
+
+
 
     }
 
